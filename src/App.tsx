4531b0031fa93c45b391afe9dbc82603f7fd2ec9
Jordan Oliveira <jordandossantosoliveira@gmail.com>
2020-10-08 21:04:26 -0300

    Configurando StatusBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import {View} from 'react-native';
+import { View, StatusBar } from 'react-native';
 
 // import { Container } from './styles';
 
-const src: React.FC = () => {
-  return <View />;
-};
+const src: React.FC = () => (
+  <>
+    <StatusBar barStyle="light-content" backgroundColor="#312e38" />
+    <View style={{ flex: 1, backgroundColor: '#312e38' }} />
+  </>
+);
 
 export default src;
