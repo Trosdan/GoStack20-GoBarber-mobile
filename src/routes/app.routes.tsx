13757ced4c5dc809827bfd8e9bb3f47353597cba
Jordan Oliveira <jordandossantosoliveira@gmail.com>
2021-01-10 12:01:32 -0300

    Criando páginas e rotas

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -3,6 +3,9 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import Dashboard from '../pages/Dashboard';
+import Profile from '../pages/Profile';
+import AppointmentCreated from '../pages/AppointmentCreated';
+import CreateAppointment from '../pages/CreateAppointment';
 
 const App = createStackNavigator();
 
@@ -13,7 +16,11 @@ const AppRoutes: React.FC = () => (
       cardStyle: { backgroundColor: '#312e38' },
     }}
   >
-    <App.Screen name="SignDashboardUp" component={Dashboard} />
+    <App.Screen name="Dashboard" component={Dashboard} />
+    <App.Screen name="AppointmentCreated" component={AppointmentCreated} />
+    <App.Screen name="CreateAppointment" component={CreateAppointment} />
+
+    <App.Screen name="Profile" component={Profile} />
   </App.Navigator>
 );
 
