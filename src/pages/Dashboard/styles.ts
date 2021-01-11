82c94eb43f70b3e5fe0fd8036e473008d6a764af
Jordan Oliveira <jordandossantosoliveira@gmail.com>
2021-01-11 20:57:59 -0300

    Header do Dashboard

--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,3 +1,41 @@
-import styled from 'styled-components/native';
+import styled, { css } from 'styled-components/native';
+import { Platform } from 'react-native';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  flex: 1;
+`;
+
+export const Header = styled.View`
+  padding: 24px;
+  background: #28262e;
+
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+
+  ${() =>
+    Platform.OS === 'ios' &&
+    css`
+      padding-top: ${getStatusBarHeight() + 24}px;
+    `}
+`;
+
+export const HeaderTitle = styled.Text`
+  color: #f4ede8;
+  font-size: 20px;
+  font-family: 'RobotoSlab-Reguler';
+  line-height: 28px;
+`;
+export const UserName = styled.Text`
+  color: #ff9000;
+  font-family: 'RobotoSlab-Medium';
+`;
+
+export const ProfileButton = styled.TouchableOpacity``;
+
+export const UserAvatar = styled.Image`
+  height: 56px;
+  width: 56px;
+  border-radius: 28px;
+`;
